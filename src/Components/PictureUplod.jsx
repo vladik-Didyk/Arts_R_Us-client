9935a10fdde71d245f8bccd6ce520f8f3c4c03c8
Juli-h <guliag4@gmail.com>
2021-05-10 19:04:20 +0300

    Create PictureUplod.jsx

---
+++ src/Components/PictureUplod.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PictureUplod = () => {
+  return (
+    <div>
+      <h1>im main component</h1>
+      <p>description about the app and how to use it</p>
+      {/* insert component here */}
+    </div>
+  );
+};
+
+export default PictureUplod;
